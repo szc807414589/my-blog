9fa66ad2a973fd439b00060d2531af78ed9c5769
szc807414589 <807414589@qq.com>
2018-12-06 20:48:45 +0800

    11

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react'
-import {Input, Menu, Dropdown, Icon, Breadcrumb, Switch, Button} from 'antd';
+import {Input, Menu, Dropdown, Icon, Breadcrumb, Switch, Button, Layout} from 'antd';
 import './header.less'
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+const {Header, Content, Footer} = Layout;
 
 const menu = (
 	<Menu>
@@ -31,37 +34,57 @@ export default class HeaderBar extends Component {
 	
 	render() {
 		return (
-			<div className="sHeader">
-				{/*logo*/}
-				<h1>LOGO</h1>
-				{/*tab*/}
-				<ul>
-					<li>首页</li>
-					<li>About</li>
-					<li>分类</li>
-				</ul>
-				{/*搜索*/}
-				<div>
-					<Input type="text" placeholder="search"/>
-				</div>
-				{/*换主题*/}
-				<div>
-					<Dropdown
-						overlay={menu}
-						onVisibleChange={this.handleVisibleChange}
-						trigger={['click']}
-						visible={this.state.visible}
-					>
-						<a className="ant-dropdown-link" href="#">
-							Click me <Icon type="down"/>
-						</a>
-					</Dropdown>
-				</div>
-				{/*发表文章
-				  *需要登录
-				  * */}
-				<Button type="primary" ghost>登录</Button>
-			</div>
+			<Layout className="layout">
+				<Header>
+					<div className="header_container">
+						{/*logo*/}
+						<a className="header_logo">LOGO</a>
+						{/*tab*/}
+						{/*<ul className="header_tab">*/}
+							{/*<li>首页</li>*/}
+							{/*<li>About</li>*/}
+							{/*<li>分类</li>*/}
+						{/*</ul>*/}
+						<Menu
+							onClick={this.handleClick}
+							selectedKeys={[this.state.current]}
+							mode="horizontal"
+						>
+							<Menu.Item key="mail">
+								<Icon type="mail" />首页
+							</Menu.Item>
+							<Menu.Item key="app">
+								<Icon type="appstore" />分类
+							</Menu.Item>
+							<Menu.Item key="alipay">
+								About
+							</Menu.Item>
+						</Menu>
+						{/*搜索*/}
+						<div className="header_form">
+							<Input type="text" placeholder="search"/>
+						</div>
+						{/*换主题*/}
+						<div className="header_menu">
+							<Dropdown
+								overlay={menu}
+								onVisibleChange={this.handleVisibleChange}
+								trigger={['click']}
+								visible={this.state.visible}
+							>
+								<a className="ant-dropdown-link" href="#">
+									Click me <Icon type="down"/>
+								</a>
+							</Dropdown>
+						</div>
+						{/*发表文章
+					 	  *需要登录
+					 	  * */}
+						<Button className="header_login" type="primary" ghost>登录</Button>
+					</div>
+				</Header>
+			</Layout>
+		
 		)
 	}
 }
