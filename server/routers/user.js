c116d508c0d8f0aeb3e6996eb8036b0eef714926
szc807414589 <807414589@qq.com>
2019-01-03 17:34:35 +0800

    修改用户信息

--- server/routers/user.js
+++ server/routers/user.js
@@ -3,7 +3,7 @@ import codeData from '../code'
 import utility from 'utility'
 import BaseComponent from '../utils'
 
-const _filter = { 'pwd': 0, __v: 0,_id:0 }
+const _filter = { 'pwd': 0, __v: 0, _id: 0 }
 
 //md5加密
 
@@ -26,14 +26,34 @@ class user extends BaseComponent {
 			return res.json({ code: 10003, msg: codeData[10003] })
 		}
 		User.findOne({ _id: userKey }, _filter, (err, doc) => {
-			// console.log(err)
 			if (err) {
-				return res.json({ code: 99999, msg: codeData[99999],data:null })
+				return res.json({ code: 99999, msg: codeData[99999], data: null })
 			}
 			if (doc) {
-				return res.json({ code: 10000, msg: codeData[10000],data:doc })
+				return res.json({ code: 10000, msg: codeData[10000], data: doc })
 			}
 		})
+	}
+	
+	modifyUserInfo(req, res) {
+		const { userKey } = req.cookies
+		const body = req.body
+		console.log('-----------------------')
+		console.log(body)
+		console.log('-----------------------')
+		User.findByIdAndUpdate(
+			{ _id: userKey },
+			body,
+			{ new: true },
+			(err, doc) => {
+				console.log(doc)
+				if (err) {
+					return res.json({ code: 10005, msg: codeData[10005], data: null })
+				}
+				if (doc) {
+					return res.json({ code: 10000, msg: codeData[10000], data: doc })
+				}
+			})
 	}
 	
 	async register(req, res) {
@@ -50,30 +70,30 @@ class user extends BaseComponent {
 			return
 		}
 		const { user, pwd } = req.body
-		User.findOne({ user }, async (err, doc) => {
+		User.findOne({ user }, { 'pwd': 0, __v: 0 }, async (err, doc) => {
 			if (doc) {
 				return res.json({ code: 10001, msg: codeData[10001] })
 			}
 			const userModel = new User({ user, pwd: that.md5Pwd(pwd), userId })
-			userModel.save( (e, d) => {
+			userModel.save((e, d) => {
 				if (e) {
 					return res.json({ code: 99999, msg: codeData[99999] })
 				}
 				const { _id } = d
 				res.cookie('userKey', _id)
-				return res.json({ code: 10000, msg: codeData[10000] })
+				return res.json({ code: 10000, msg: codeData[10000], data: d })
 			})
 		})
 	}
 	
 	login(req, res) {
 		const { user, pwd } = req.body
-		User.findOne({ user, pwd: this.md5Pwd(pwd) }, (err, doc) => {
+		User.findOne({ user, pwd: this.md5Pwd(pwd) }, { 'pwd': 0, __v: 0 }, (err, doc) => {
 			if (!doc) {
 				return res.json({ code: 10004, msg: codeData[10004] })
 			}
 			res.cookie('userKey', doc._id)
-			return res.json({ code: 10000, msg: codeData[10000] })
+			return res.json({ code: 10000, msg: codeData[10000], data: doc })
 		})
 	}
 	
@@ -82,7 +102,6 @@ class user extends BaseComponent {
 		return utility.md5(utility.md5(pwd + salt))
 	}
 }
-
 
 
 module.exports = new user()
--- server/routers/v1/user.js
+++ server/routers/v1/user.js
@@ -6,6 +6,6 @@ router.get('/user/list',user.list)
 router.post('/user/userInfo',user.userInfo)
 router.post('/user/register',user.register)
 router.post('/user/login',user.login)
+router.post('/user/modifyUserInfo',user.modifyUserInfo)
 
 module.exports = router
-// export default router
